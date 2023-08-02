07c583d0ccc213668f793cc98bc62d53c180d548
Lokesh b s <lokeshbs0608@gmail.com>
2023-08-02 08:18:16 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Pooja_Pal',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
